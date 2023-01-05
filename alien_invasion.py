e16ec29e837f0351a6890139bb0697038d892132
Konrad Łapka <konrad.lapka58@hotmail.com>
2023-01-05 03:07:28 +0100

    Add ship.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -16,18 +17,29 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
     def run_game(self) -> None:
         """Start the main loop of the game."""
 
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    raise SystemExit()
+            self._check_events()
+            self._update_screen()
 
-            self.screen.fill(self.settings.bg_color)
+    def _check_events(self):
+        """Respond to keypresses and mouses events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                raise SystemExit()
 
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+    def _update_screen(self):
+        """Update images on the screen and flip to the new screen."""
+
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
